d2719d5e65d2f91dcf4cd8a82da71c43600a8850
Ugo LeviCescutti <ugolc@localhost.localdomain>
2020-05-19 22:55:59 +0200

    [ADD] Add HelloWorld main and CMakeList for Linux

---
+++ build/CMakeFiles/bomberman.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ugolc/SEMESTRE_4/INDIE STUDIO/OOP_indie_studio_2019/main.cpp" "/home/ugolc/SEMESTRE_4/INDIE STUDIO/OOP_indie_studio_2019/build/CMakeFiles/bomberman.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../lib/irrlicht-1.8.4/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
