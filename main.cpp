bbd695b97c624d824dd04136742fdd748c0fe405
Arthur Benard <abenardFix@arthurFix.localdomain>
2020-05-24 22:07:42 +0200

    [ADD] Irrlicht given folder for windows

--- main.cpp
+++ main.cpp
@@ -1,20 +1,4 @@
 #include <irrlicht.h>
-
-using namespace irr;
-using namespace core;
-using namespace scene;
-using namespace video;
-using namespace io;
-using namespace gui;
-
-/*
-To be able to use the Irrlicht.DLL file, we need to link with the Irrlicht.lib.
-We could set this option in the project settings, but to make it easy, we use a
-pragma comment lib for VisualStudio. On Windows platforms, we have to get rid
-of the console window, which pops up when starting a program with main(). This
-is done by the second pragma. We could also use the WinMain method, though
-losing platform independence then.
-*/
 #ifdef _IRR_WINDOWS_
 #pragma comment(lib, "Irrlicht.lib")
 #pragma comment(linker, "/subsystem:windows /ENTRY:mainCRTStartup")
@@ -23,128 +7,32 @@ losing platform independence then.
 
 int main()
 {
-	/*
-	- deviceType: Type of the device. This can currently be the Null-device,
-	   one of the two software renderers, D3D8, D3D9, or OpenGL. In this
-	   example we use EDT_SOFTWARE, but to try out, you might want to
-	   change it to EDT_BURNINGSVIDEO, EDT_NULL, EDT_DIRECT3D8,
-	   EDT_DIRECT3D9, or EDT_OPENGL.
-
-	- windowSize: Size of the Window or screen in FullScreenMode to be
-	   created. In this example we use 640x480.
-
-	- bits: Amount of color bits per pixel. This should be 16 or 32. The
-	   parameter is often ignored when running in windowed mode.
-
-	- fullscreen: Specifies if we want the device to run in fullscreen mode
-	   or not.
-
-	- stencilbuffer: Specifies if we want to use the stencil buffer (for
-	   drawing shadows).
-
-	- vsync: Specifies if we want to have vsync enabled, this is only useful
-	   in fullscreen mode.
-
-	- eventReceiver: An object to receive events. We do not want to use this
-	   parameter here, and set it to 0.
-	*/
-	IrrlichtDevice *device = createDevice( video::EDT_SOFTWARE, dimension2d<u32>(640, 480), 16, false, false, false, 0);
+	irr::IrrlichtDevice *device = irr::createDevice( irr::video::EDT_SOFTWARE, irr::core::dimension2d<irr::u32>(640, 480), 16, false, false, false, 0);
 	if (!device)
-		return 1;
+		return (1);
 	device->setWindowCaption(L"Hello World! - Irrlicht Engine Demo");
-
-	/*
-	Get a pointer to the VideoDriver, the SceneManager and the graphical
-	user interface environment, so that we do not always have to write
-	device->getVideoDriver(), device->getSceneManager(), or
-	device->getGUIEnvironment().
-	*/
-	IVideoDriver* driver = device->getVideoDriver();
-	ISceneManager* smgr = device->getSceneManager();
-	IGUIEnvironment* guienv = device->getGUIEnvironment();
-
-	/*
-	We add a hello world label to the window, using the GUI environment.
-	The text is placed at the position (10,10) as top left corner and
-	(260,22) as lower right corner.
-	*/
-	guienv->addStaticText(L"Hello World! This is the Irrlicht Software renderer!",
-		rect<s32>(10,10,260,22), true);
-
-	/*
-	To show something interesting, we load a Quake 2 model and display it.
-	We only have to get the Mesh from the Scene Manager with getMesh() and add
-	a SceneNode to display the mesh with addAnimatedMeshSceneNode(). We
-	check the return value of getMesh() to become aware of loading problems
-	and other errors.
-
-	Instead of writing the filename sydney.md2, it would also be possible
-	to load a Maya object file (.obj), a complete Quake3 map (.bsp) or any
-	other supported file format. By the way, that cool Quake 2 model
-	called sydney was modelled by Brian Collins.
-	*/
-	IAnimatedMesh* mesh = smgr->getMesh("assets/sydney.md2");
-	if (!mesh)
-	{
+	irr::video::IVideoDriver* driver = device->getVideoDriver();
+	irr::scene::ISceneManager* smgr = device->getSceneManager();
+	irr::gui::IGUIEnvironment* guienv = device->getGUIEnvironment();
+	guienv->addStaticText(L"Hello World! This is the Irrlicht Software renderer!", irr::core::rect<irr::s32>(10,10,260,22), true);
+	irr::scene::IAnimatedMesh* mesh = smgr->getMesh("assets/sydney.md2");
+	if (!mesh) {
 		device->drop();
-		return 1;
+		return (1);
 	}
-	IAnimatedMeshSceneNode* node = smgr->addAnimatedMeshSceneNode( mesh );
-
-	/*
-	To let the mesh look a little bit nicer, we change its material. We
-	disable lighting because we do not have a dynamic light in here, and
-	the mesh would be totally black otherwise. Then we set the frame loop,
-	such that the predefined STAND animation is used. And last, we apply a
-	texture to the mesh. Without it the mesh would be drawn using only a
-	color.
-	*/
-	if (node)
-	{
-		node->setMaterialFlag(EMF_LIGHTING, false);
-		node->setMD2Animation(scene::EMAT_STAND);
+	irr::scene::IAnimatedMeshSceneNode* node = smgr->addAnimatedMeshSceneNode(mesh);
+	if (node) {
+		node->setMaterialFlag(irr::video::EMF_LIGHTING, false);
+		node->setMD2Animation(irr::scene::EMAT_STAND);
 		node->setMaterialTexture( 0, driver->getTexture("assets/sydney.bmp") );
 	}
-
-	/*
-	To look at the mesh, we place a camera into 3d space at the position
-	(0, 30, -40). The camera looks from there to (0,5,0), which is
-	approximately the place where our md2 model is.
-	*/
-	smgr->addCameraSceneNode(0, vector3df(0,30,-40), vector3df(0,5,0));
-
-	/*
-	Ok, now we have set up the scene, lets draw everything: We run the
-	device in a while() loop, until the device does not want to run any
-	more. This would be when the user closes the window or presses ALT+F4
-	(or whatever keycode closes a window).
-	*/
-	while(device->run())
-	{
-		/*
-		Anything can be drawn between a beginScene() and an endScene()
-		call. The beginScene() call clears the screen with a color and
-		the depth buffer, if desired. Then we let the Scene Manager and
-		the GUI Environment draw their content. With the endScene()
-		call everything is presented on the screen.
-		*/
-		driver->beginScene(true, true, SColor(255,100,101,140));
-
+	smgr->addCameraSceneNode(0, irr::core::vector3df(0,30,-40), irr::core::vector3df(0,5,0));
+	while(device->run()) {
+		driver->beginScene(true, true, irr::video::SColor(255,100,101,140));
 		smgr->drawAll();
 		guienv->drawAll();
-
 		driver->endScene();
 	}
-
-	/*
-	After we are done with the render loop, we have to delete the Irrlicht
-	Device created before with createDevice(). In the Irrlicht Engine, you
-	have to delete all objects you created with a method or function which
-	starts with 'create'. The object is simply deleted by calling ->drop().
-	See the documentation at irr::IReferenceCounted::drop() for more
-	information.
-	*/
 	device->drop();
-
-	return 0;
+	return (0);
 }
