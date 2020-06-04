aab193780d8f38f91d8a7bb7296d9245af2f8c41
arthur <arthur.benard@epitech.com>
2020-06-04 22:46:44 +0200

    [FIX] Wall.cpp Floor.cpp Obstacle.cpp useless comment

--- src/Object/Floor.cpp
+++ src/Object/Floor.cpp
@@ -12,7 +12,6 @@ Floor::Floor(float posx, float posy, scene::ISceneManager* smgr, video::IVideoDr
     scene::IAnimatedMesh* mesh = smgr->getMesh("assets/textures/box.MD3");
     _type = ObjectType::Floor;
     _node = smgr->addAnimatedMeshSceneNode(mesh);
-    //_pos = irr::core::vector3df(posx, 8, posy);
     _pos = {posx, 0, posy};
     _node->setPosition(_pos);
     _node->setMaterialFlag(irr::video::EMF_LIGHTING, false);
--- src/Object/Obstacle.cpp
+++ src/Object/Obstacle.cpp
@@ -12,7 +12,6 @@ Obstacle::Obstacle(float posx, float posy, scene::ISceneManager* smgr, video::IV
     scene::IAnimatedMesh* mesh = smgr->getMesh("assets/textures/box.MD3");
     _type = ObjectType::Obstacle;
     _node = smgr->addAnimatedMeshSceneNode(mesh);
-    //_pos = irr::core::vector3df(posx, 8, posy);
     _pos = {posx, 40, posy};
     _node->setPosition(_pos);
     _node->setMaterialFlag(irr::video::EMF_LIGHTING, false);
