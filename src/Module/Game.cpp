513bd784b64d8b85674f8bc5f50b4db6e0c9efb9
arthur <arthur.benard@epitech.com>
2020-06-04 22:23:52 +0200

    [FIX] Map.cpp/hpp replace old box by new box. old = 8x8; new = 40x40

--- src/Module/Game.cpp
+++ src/Module/Game.cpp
@@ -27,7 +27,7 @@ void Game::Loop(std::vector<std::shared_ptr<IModule>> obj)
     Map mapBomber(core->smgr, core->driver);
     core->driver->getMaterial2D().TextureLayer[0].BilinearFilter=true;
     core->driver->getMaterial2D().AntiAliasing=video::EAAM_FULL_BASIC;
-    core->smgr->addCameraSceneNode(0, irr::core::vector3df(-20,250,-20), irr::core::vector3df(104,0,136));
+    core->smgr->addCameraSceneNode(0, irr::core::vector3df(340,400,-60), irr::core::vector3df(340,0,340));
     while(core->device->run() && core->driver) {
         if (core->device->isWindowActive()) {
             core->driver->beginScene(true, true, video::SColor(0,0,0,0));
--- src/Object/Wall.cpp
+++ src/Object/Wall.cpp
@@ -9,14 +9,14 @@
 
 Wall::Wall(float posx, float posy, scene::ISceneManager* smgr, video::IVideoDriver* driver)
 {
-    scene::IAnimatedMesh* mesh = smgr->getMesh("assets/textures/Block/ghost.obj");
+    scene::IAnimatedMesh* mesh = smgr->getMesh("assets/textures/box.MD3");
     _type = ObjectType::Wall;
     _node = smgr->addAnimatedMeshSceneNode(mesh);
     //_pos = irr::core::vector3df(posx, 8, posy);
-    _pos = {posx, 8, posy};
+    _pos = {posx, 40, posy};
     _node->setPosition(_pos);
     _node->setMaterialFlag(irr::video::EMF_LIGHTING, false);
-    _node->setMaterialTexture(0, driver->getTexture("assets/textures/Block/bedrock.png"));
+    _node->setMaterialTexture(0, driver->getTexture("assets/textures/brique.jpg"));
 }
 
 Wall::~Wall()
